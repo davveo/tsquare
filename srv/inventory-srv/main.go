d7e1e56f01fdea184e6f4c66338f4f891fcaa287
davve <davve@qq.com>
2020-05-13 13:37:07 +0800

    gen base srv

---
+++ srv/inventory-srv/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2"
+	"inventory-srv/handler"
+	"inventory-srv/subscriber"
+
+	inventory "inventory-srv/proto/inventory"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.service.inventory"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	inventory.RegisterInventoryHandler(service.Server(), new(handler.Inventory))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("go.micro.service.inventory", service.Server(), new(subscriber.Inventory))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}

dbcf9c4ae9fdefb0e502ea61945d52440cc50864
davve <davve@qq.com>
2020-05-14 10:28:57 +0800

    auth srv

---
+++ srv/auth-srv/go.mod
@@ -0,0 +1,5 @@
+module auth-srv
+
+go 1.13
+
+require github.com/micro/go-micro/v2 v2.6.0
---
+++ srv/auth-srv/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2"
+	"auth-srv/handler"
+	"auth-srv/subscriber"
+
+	auth "github.com/zbrechave/tsquare/auth-srv/proto/auth"
+)
+
+func main() {
+	// New Service
+	service := micro.NewService(
+		micro.Name("go.micro.service.auth"),
+		micro.Version("latest"),
+	)
+
+	// Initialise service
+	service.Init()
+
+	// Register Handler
+	auth.RegisterAuthHandler(service.Server(), new(handler.Auth))
+
+	// Register Struct as Subscriber
+	micro.RegisterSubscriber("go.micro.service.auth", service.Server(), new(subscriber.Auth))
+
+	// Run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}

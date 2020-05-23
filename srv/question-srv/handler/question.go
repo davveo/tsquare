8e8bdffc5e3029576f516b74e78afa5e44da13f9
davve <921971751@qq.com>
2020-05-23 15:49:41 +0800

    修改配置

--- srv/question-srv/handler/question.go
+++ srv/question-srv/handler/question.go
@@ -2,16 +2,15 @@ package handler
 
 import (
 	"context"
-	proto "github.com/zbrechave/tsquare/srv/question-srv/proto/question"
 	log "github.com/micro/go-micro/v2/logger"
+	proto "github.com/zbrechave/tsquare/srv/question-srv/proto/question"
 )
 
 type QuestionService struct{}
 
-func Init()  {
-	queService, _ =
-}
+func Init() {
 
+}
 
 func (qs *QuestionService) CreateQuestion(ctx context.Context, req *proto.Request, resp *proto.Response) (err error) {
 	log.Info("[收到创建请求 in CreateQuestion]")

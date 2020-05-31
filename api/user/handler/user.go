226ef4ed54ed0e6921d9293a75f2d5b042c88eb8
davve <921971751@qq.com>
2020-05-31 20:19:27 +0800

    fix stru

--- api/user/handler/user.go
+++ api/user/handler/user.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"context"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro/v2/client"
 	"github.com/zbrechave/tsquare/basic/common"
@@ -21,7 +20,6 @@ import (
 var (
 	userService user.UserService
 	authService auth.AuthService
-
 )
 
 type User struct{}
@@ -48,49 +46,29 @@ func Init()  {
 }
 
 
-func (e *User) Login(ctx *gin.Context) {
+func (u *User) Login(ctx *gin.Context) {
 	log.Info("Received User.Login request")
 
-	//userClient, ok := srvClient.UserFromContext(ctx)
-	//if !ok {
-	//	err := errors.InternalServerError("go.micro.srv.user", "user client not found")
-	//	ctx.JSON(http.StatusInternalServerError, err)
-	//	return
-	//}
-	//
-	//authClient, ok := srvClient.AuthFromContext(ctx)
-	//if !ok {
-	//	err := errors.InternalServerError("go.micro.srv.auth", "srv client not found")
-	//	ctx.JSON(http.StatusInternalServerError, err)
-	//	return
-	//}
 	var loginReq LoginRequest
-
 	if err := ctx.BindJSON(&loginReq); err != nil {
-		log.Warnf("bind param err: %v", err)
-		return
+		log.Errorf("bind param err: %v", err)
+		ctx.JSON(http.StatusInternalServerError, err.Error())
 	}
 
-
-	fmt.Println(loginReq.UserName, loginReq.PassWord)
-
-	userName := loginReq.UserName
-	password := loginReq.PassWord
-
 	resp, err := userService.QueryUserByName(context.TODO(), &user.Request{
-		UserName: userName,
+		UserName: loginReq.UserName,
 	})
 
 	if err != nil {
+		log.Errorf("call userService.QueryUserByName err: %v", err)
 		ctx.JSON(http.StatusInternalServerError, err.Error())
-		return
 	}
 
 	response := map[string]interface{}{
 		"ref": time.Now().UnixNano(),
 	}
 
-	if resp.User.Pwd == password {
+	if resp.User.Pwd == loginReq.PassWord {
 		resp.User.Pwd = ""
 		response["success"] = true
 		response["data"] = resp.User
@@ -133,88 +111,32 @@ func (e *User) Login(ctx *gin.Context) {
 }
 
 
-func (e *User) Logout(c *gin.Context) {
-//	log.Info("Received Say.Hello API request")
-//
-//	name := c.Param("name")
-//
-//	response, err := cl.Hello(context.TODO(), &hello.Request{
-//		Name: name,
-//	})
-//
-//	if err != nil {
-//		c.JSON(500, err)
-//	}
-//
-//	c.JSON(200, response)
-//
-//	if r.Method != "POST" {
-//		log.Infof("非法请求")
-//		http.Error(w, "非法请求", 400)
-//		return
-//	}
-//	_ = r.ParseForm()
-//
-//	rsp, err := userClient.QueryUserByName(context.TODO(), &user.Request{
-//		UserName: r.Form.Get("userName"),
-//	})
-//	if err != nil {
-//		http.Error(w, err.Error(), 500)
-//		return
-//	}
-//	// 返回结果
-//	response := map[string]interface{}{
-//		"ref": time.Now().UnixNano(),
-//	}
-//
-//	if rsp.User.Pwd == r.Form.Get("pwd") {
-//		response["success"] = true
-//
-//		// 干掉密码返回
-//		rsp.User.Pwd = ""
-//		response["data"] = rsp.User
-//		log.Infof("[Login] 密码校验完成，生成token...")
-//
-//		// 生成token
-//		rsp2, err := authClient.MakeAccessToken(context.TODO(), &auth.Request{
-//			UserId:   rsp.User.Id,
-//			UserName: rsp.User.Name,
-//		})
-//		if err != nil {
-//			log.Errorf("[Login] 创建token失败，err：%s", err)
-//			http.Error(w, err.Error(), 500)
-//			return
-//		}
-//
-//		log.Errorf("[Login] token %s", rsp2.Token)
-//		response["token"] = rsp2.Token
-//
-//		// 同时将token写到cookies中
-//		w.Header().Add("set-cookie", "application/json; charset=utf-8")
-//		// 过期30分钟
-//		expire := time.Now().Add(30 * time.Minute)
-//		cookie := http.Cookie{Name: "remember-me-token", Value: rsp2.Token, Path: "/", Expires: expire, MaxAge: 90000}
-//		http.SetCookie(w, &cookie)
-//
-//		timeString := time.Now().Format(common.DefaultMsTimeLayout)
-//
-//		// 同步到session中
-//		sess := session.GetSession(w, r)
-//		sess.Values["userId"] = rsp.User.Id
-//		sess.Values["userName"] = rsp.User.Name
-//		sess.Values["login_time"] = timeString
-//		_ = sess.Save(r, w)
-//
-//	} else {
-//		response["success"] = false
-//		response["error"] = &Error{
-//			Detail: "密码错误",
-//		}
-//	}
-//	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-//	// 返回JSON结构
-//	if err := json.NewEncoder(w).Encode(response); err != nil {
-//		http.Error(w, err.Error(), 500)
-//		return
-//	}
+func (u *User) Logout(ctx *gin.Context) {
+	log.Info("Received User.Logout request")
+
+	tokenVal, err := ctx.Cookie("remember-me-token")
+	if err != nil {
+		log.Error("token获取失败")
+		ctx.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	_, err = authService.DelUserAccessToken(context.TODO(), &auth.Request{
+		Token: tokenVal,
+	})
+	if err != nil {
+		log.Errorf("call authService.DelUserAccessToken err: %v", err)
+		ctx.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	ctx.SetCookie(
+		"remember-me-token",
+		"", 0,
+		"/", "localhost",
+		false, true,
+	)
+	response := map[string]interface{}{
+		"ref":     time.Now().UnixNano(),
+		"success": true,
+	}
+	ctx.JSON(http.StatusOK, response)
 }

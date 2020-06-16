73b405120b1348ece1cc3ac454f1d7c1c5f0f3db
davve <921971751@qq.com>
2020-06-16 21:34:58 +0800

    senword

--- srv/senword/handler/senword.go
+++ srv/senword/handler/senword.go
@@ -6,6 +6,7 @@ import (
 	log "github.com/micro/go-micro/v2/logger"
 
 	senword "github.com/zbrechave/tsquare/srv/senword/proto/senword"
+	utils "github.com/zbrechave/tsquare/srv/senword/utils"
 )
 
 type Senword struct{}
@@ -13,6 +14,11 @@ type Senword struct{}
 // Call is a single request handler called via client.Call or the generated client code
 func (e *Senword) Call(ctx context.Context, req *senword.Request, rsp *senword.Response) error {
 	log.Info("Received Senword.Call request")
+	//var tree utils.Trie
+	//var filterbyte [][]byte
+	//tree.InitRootNode()
+	//tree.BuildTrie(filterbyte)
+
 
 	return nil
 }
